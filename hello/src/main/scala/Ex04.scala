dc0553f9461f7fd025b309c6675b0287d5b40322
aloni636 <elyasaf.aloni@gmail.com>
2025-12-09 03:45:23 +0000

    feat: add http requests using HttpClient

--- hello/src/main/scala/Ex04.scala
+++ hello/src/main/scala/Ex04.scala
@@ -6,11 +6,17 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.blocking
 import scala.concurrent.TimeoutException
-// IO
+// File IO
 import java.nio.file.Files
 import java.nio.file.Paths
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+// Network IO
+// See: https://www.baeldung.com/java-9-http-client#api
+import java.net.http.{HttpClient, HttpRequest, HttpResponse}
+import java.net.URI
+import scala.concurrent.{ExecutionContext, Future}
+import scala.jdk.FutureConverters._ // Provides .asScala implicit conversions
 
 /*
   ---
@@ -51,6 +57,7 @@ object Ex04 extends Exercise {
       println(s"[${userNumbers.mkString(", ")}]")
     } catch { case _: TimeoutException => println("Timeout") }
 
+    // Async filesystem IO
     val tiffPaths = List(
       "../data/0x00000001-0x00000002-0x00000001.tiff",
       "../data/0x00000001-0x00000002-0x00000003.tiff"
@@ -62,9 +69,54 @@ object Ex04 extends Exercise {
     }
     // `gather` like synchronization
     val tiffFutures = Future.traverse(tiffPaths)(scheduleTiffRead)
-    val printFuture = tiffFutures.map { x => x.foreach(println(_)) }
+    val printTiffFuture = tiffFutures.map { x => x.foreach(println(_)) }
 
     // `asyncio.run` top level execution
-    Await.result(printFuture, Duration.Inf)
+    Await.result(printTiffFuture, Duration.Inf)
+
+    // Async network IO
+    val urls = List(
+      // Global Sites
+      "https://www.google.com",
+      "https://www.cloudflare.com",
+      "https://www.amazon.com",
+      "https://www.microsoft.com",
+      "https://www.wikipedia.org",
+      "https://www.apple.com",
+      // Dev Focused
+      "https://github.com",
+      "https://api.github.com",
+      "https://stackoverflow.com",
+      "https://hub.docker.com",
+      "https://maven.org" // redirected to https://central.sonatype.com/?smo=true
+    )
+    val requests = urls.map { url =>
+      HttpRequest
+        .newBuilder()
+        .uri(new URI(url))
+        .GET()
+        .build()
+    }
+    val client =
+      HttpClient
+        .newBuilder()
+        .followRedirects(
+          HttpClient.Redirect.NORMAL
+        ) // the default, but I want it to be explicit because we have redirect in target urls
+        .build()
+    val requestsFuture = Future.traverse(requests) { request =>
+      log.info(
+        s"Sending GET request to '${request.uri()}'"
+      ) // WARNING: log does not participate in implicit mechanism and cannot be controlled from a central implicit location
+      client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).asScala
+    }
+    println(s"Sending GET requests to ${urls.length} urls asynchronously...")
+    val responses = Await.result(requestsFuture, Duration.Inf)
+    println(s"Done!")
+    responses.foreach { response =>
+      println(
+        s"'${response.uri()}': {status: ${response.statusCode()}, body_size: ${response.body().length()}}"
+      )
+    }
   }
 }
